cd0d1e4ed559d98479374b46276bdb5ae02543f3
Jack 1575 <jack1575@proton.me>
2024-08-14 06:49:39 -0700

    Cleanup

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var showdown = require("https://cdnjs.cloudflare.com/ajax/libs/showdown/2.1.0/showdown.min.js");
 function toggle(element, button, txt) {
     if (document.getElementById(element).style.display === "") {
         document.getElementById(element).style.display = "none";
@@ -10,10 +7,4 @@ function toggle(element, button, txt) {
         document.getElementById(element).style.display = "";
         document.getElementById(button).innerHTML = " " + txt;
     }
-}
-function readMd(file, content) {
-    var reader = new FileReader();
-    reader.readAsText(file);
-    var converter = new showdown.Converter(), text = reader.result, html = converter.makeHtml(text);
-    document.getElementById(content).innerHTML = converter.html;
 }
