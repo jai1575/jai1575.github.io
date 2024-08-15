8797dd83c63fa60f7005b94db4a0109fa76f6497
Jack 1575 <jack1575@proton.me>
2024-08-15 12:55:10 -0700

    Made site dynamic

--- script.ts
+++ script.ts
@@ -1,15 +1,18 @@
-function toggle(element, button, txt) {
+function toggle(element, button, txt, ico) {
   var x = document;
   if (x.getElementById(element).style.display === "") {
     x.getElementById(element).style.display="none";
     x.getElementById(button).innerHTML=txt;
+    x.getElementById(button+"-mob").innerHTML=ico;
   } else if (x.getElementById(element).style.display === "none") {
     x.getElementById(element).style.display="";
     x.getElementById(button).innerHTML=" "+txt;
+    x.getElementById(button+"-mob").innerHTML=" "+ico;
   }
   if (x.getElementById("csjournal").style.display==="none" &&
-      document.getElementById("devblog").style.display==="none" &&
-      document.getElementById("projects").style.display==="none") {
+      x.getElementById("devblog").style.display==="none" &&
+      x.getElementById("abtme").style.display==="none" &&
+      x.getElementById("projects").style.display==="none") {
     x.getElementById("select").style.display="block";
   } else { x.getElementById("select").style.display="none"; }
 }
