58491827adf0076fd00c4689c6858121af443a86
Jack 1575 <jack1575@proton.me>
2024-08-16 19:06:05 -0700

    Page logic update

--- script.ts
+++ script.ts
@@ -1,24 +1,36 @@
-function toggle(element, button, txt, ico) {
-  var x = document;
-  if (x.getElementById(element).style.display === "") {
-    x.getElementById(element).style.display="none";
-    x.getElementById(button).innerHTML=txt;
-    x.getElementById(button+"-mob").innerHTML=ico;
-  } else if (x.getElementById(element).style.display === "none") {
-    x.getElementById(element).style.display="";
-    x.getElementById(button).innerHTML="Close";
-    x.getElementById(button+"-mob").innerHTML="";
-  }
-  if (x.getElementById("csjournal").style.display==="none" &&
-      x.getElementById("devblog").style.display==="none" &&
-      x.getElementById("abtme").style.display==="none" &&
-      x.getElementById("projects").style.display==="none") {
-    x.getElementById("select").style.display="block";
-    x.getElementById("home-button").innerHTML=" Home"
-    x.getElementById("home-button-mob").innerHTML=" "
-  } else {
-    x.getElementById("select").style.display="none";
-    x.getElementById("home-button").innerHTML="Home"
-    x.getElementById("home-button-mob").innerHTML=""
+const home: string = "home";
+const abtm: string = "abtme";
+const csjo: string = "csjournal";
+const devb: string = "devblog";
+const proj: string = "projects";
+
+const home_txt: string = "Home";
+const abtm_txt: string = "About Me";
+const csjo_txt: string = "CS Journal";
+const devb_txt: string = "Dev Blog";
+const proj_txt: string = "Projects";
+
+const home_ico: string = "";
+const abtm_ico: string = "";
+const csjo_ico: string = "󰂾";
+const devb_ico: string = "󱃖";
+const proj_ico: string = "";
+
+function toggle(page, button) {
+  const elements: string[] = [ home,abtm,csjo,devb,proj ];
+  const elements_ico: string[] = [ home_ico,abtm_ico,csjo_ico,devb_ico,proj_ico ];
+  const elements_txt: string[] = [ home_txt,abtm_txt,csjo_txt,devb_txt,proj_txt ];
+  var elm = document.getElementById(elements[page-1]);
+  
+  for (let i = 0; i < elements.length; i++) {
+    if (i !== page-1) {
+      document.getElementById(elements[i]).style.display = "none";
+      document.getElementById(elements[i]+"-button").innerHTML = elements_txt[i]
+      document.getElementById(elements[i]+"-button-mob").innerHTML = elements_ico[i]
+    } else {
+      document.getElementById(elements[i]).style.display = "";
+      document.getElementById(elements[page-1]+"-button").innerHTML = "Close";
+      document.getElementById(elements[page-1]+"-button-mob").innerHTML = "";
+    }
   }
 }
--- src/script.js
+++ src/script.js
@@ -1,26 +1,37 @@
-function toggle(element, button, txt, ico) {
-    var x = document;
-    if (x.getElementById(element).style.display === "") {
-        x.getElementById(element).style.display = "none";
-        x.getElementById(button).innerHTML = txt;
-        x.getElementById(button + "-mob").innerHTML = ico;
+var home = "home";
+var abtm = "abtme";
+var csjo = "csjournal";
+var devb = "devblog";
+var proj = "projects";
+var home_txt = "Home";
+var abtm_txt = "About Me";
+var csjo_txt = "CS Journal";
+var devb_txt = "Dev Blog";
+var proj_txt = "Projects";
+var home_ico = "";
+var abtm_ico = "";
+var csjo_ico = "󰂾";
+var devb_ico = "󱃖";
+var proj_ico = "";
+function toggle(page, button) {
+    var elements = [home, abtm, csjo, devb, proj];
+    var elements_ico = [home_ico, abtm_ico, csjo_ico, devb_ico, proj_ico];
+    var elements_txt = [home_txt, abtm_txt, csjo_txt, devb_txt, proj_txt];
+    var elm = document.getElementById(elements[page - 1]);
+    for (var i = 0; i < elements.length; i++) {
+        if (i !== page - 1) {
+            document.getElementById(elements[i]).style.display = "none";
+            document.getElementById(elements[i] + "-button").innerHTML = elements_txt[i];
+            document.getElementById(elements[i] + "-button-mob").innerHTML = elements_ico[i];
+        }
+        else {
+            document.getElementById(elements[i]).style.display = "";
+            document.getElementById(elements[page - 1] + "-button").innerHTML = "Close";
+            document.getElementById(elements[page - 1] + "-button-mob").innerHTML = "";
+        }
     }
-    else if (x.getElementById(element).style.display === "none") {
-        x.getElementById(element).style.display = "";
-        x.getElementById(button).innerHTML = "Close";
-        x.getElementById(button + "-mob").innerHTML = "";
-    }
-    if (x.getElementById("csjournal").style.display === "none" &&
-        x.getElementById("devblog").style.display === "none" &&
-        x.getElementById("abtme").style.display === "none" &&
-        x.getElementById("projects").style.display === "none") {
-        x.getElementById("select").style.display = "block";
-        x.getElementById("home-button").innerHTML = " Home";
-        x.getElementById("home-button-mob").innerHTML = " ";
-    }
-    else {
-        x.getElementById("select").style.display = "none";
-        x.getElementById("home-button").innerHTML = "Home";
-        x.getElementById("home-button-mob").innerHTML = "";
-    }
+    //if (elm.style.display = "none") {
+    //  btn.innerHTML = txt;
+    //  btnmob.innerHTML = ico;
+    //}
 }
